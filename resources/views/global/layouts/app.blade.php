a188ab0356ee53f29f5291dbf788042fc416d06c
Mandeep Neupane <mandeepneupane@gmail.com>
2022-01-22 22:21:10 +0545

    dadasd

--- resources/views/global/layouts/app.blade.php
+++ resources/views/global/layouts/app.blade.php
@@ -13,8 +13,8 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    {{-- <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> --}}
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
@@ -25,7 +25,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid px-5 py-2">
-            <a class="navbar-brand fs-4 fw-bold" href="#">Navbar</a>
+            <a class="navbar-brand fs-4 fw-bold" href="{{ config('app.url', 'Laravel') }}">{{ config('app.name', 'Laravel') }}</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -33,13 +33,16 @@
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="{{ config('app.url', 'Home').'/home' }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="{{ config('app.url', 'Recent-Ads') .'/recent-ads'}}">Recent Ads</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link" href="{{ config('app.url', 'About-Us') .'/about-us'}}">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ config('app.url', 'Contact-Us') .'/contact-us'}}">Contact Us</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link">Disabled</a>
