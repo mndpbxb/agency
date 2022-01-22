1788cb1132864afffc3f7c0e26bc73b0f5b4e867
Mandeep Neupane <mandeepneupane@gmail.com>
2022-01-22 22:05:20 +0545

    sda

---
+++ resources/views/global/layouts/app.blade.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid px-5 py-2">
+            <a class="navbar-brand fs-4 fw-bold" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main class="container-fluid">
+        @yield('content')
+
+    </main>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+</body>
+
+</html>
---
+++ resources/views/global/pages/home.blade.php
@@ -0,0 +1,7 @@
+
+@extends('global.layouts.app')
+@section('content')
+    <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit possimus quia praesentium quo eos asperiores expedita ex? Enim vel tenetur culpa asperiores nisi sapiente est modi aliquam ab. Voluptas aliquid ex explicabo doloribus placeat velit delectus sunt nam. Sed nulla dolorum voluptatem minus asperiores tempora, in sunt necessitatibus id dolore.
+    </p>
+@endsection
